c54400a34e60f15c91ba43802314a2ec98921c70
Jesse Stuart <hi@jessestuart.com>
2019-08-04 06:24:01 -0400

    fix(styling): Fix up footer + update snapshots.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,12 +10,19 @@ const Footer = () => {
     <footer
       className="bb bottom-0 bw2 f5-ns f6 flex flex-column fw4 lh-title pa4 w-100"
       style={{
-        borderColor: colors.primary,
+        borderBottomColor: colors.primary,
         borderTop: `1px #e4e4e4 solid`,
       }}
     >
       <div className="flex justify-center items-center">
-        © 2018–{DateTime.local().year} Jesse Stuart
+        © 2018–{DateTime.local().year}{' '}
+        <a
+          href="https://jesses.io/"
+          className="pl1 no-underline b"
+          style={{ color: 'rgba(0, 0, 0, 0.80)' }}
+        >
+          Jesse Stuart
+        </a>
       </div>
       <div className="dib center tc flex-ns justify-center items-center">
         Pixels, bits and bytes hand-crafted with
